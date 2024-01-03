a14f38e5a3b6b0abbe98e0fb8e283a2c61ffc3e1
Ayoola <ayo.oloyede16@gmail.com>
2024-01-03 15:20:06 +0100

    Fix bottom sheet gesture not working on android

--- layouts/AppLayout/components/ModalOverlay/ModalOverlay.js
+++ layouts/AppLayout/components/ModalOverlay/ModalOverlay.js
@@ -1,18 +1,21 @@
-import { TouchableWithoutFeedback, View } from "react-native";
+import { TouchableWithoutFeedback } from "react-native";
 import { appLayoutStyles } from "../../styles";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function ModalOverlay ({ children, handleClickOutside }) {
-    return <TouchableWithoutFeedback 
-            // onPress={
-            //     handleClickOutside && typeof handleClickOutside === 'function' ?
-            //         () => handleClickOutside()
-            //     :
-            //     () => {}
-            // }
-        >
-        <View  style={appLayoutStyles.modalOverlay} >
-        { children }
+    const handlePressInTouchableFeedback = (e) => {
 
-        </View>
+        if (handleClickOutside && typeof handleClickOutside === 'function') {
+        
+        }
+        
+    }
+
+    return <TouchableWithoutFeedback 
+        onPress={handlePressInTouchableFeedback}
+    >
+        <GestureHandlerRootView style={appLayoutStyles.modalOverlay}>
+            { children }
+        </GestureHandlerRootView>
     </TouchableWithoutFeedback>
 }
