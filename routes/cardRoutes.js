d56501cf3456e7bc0d145b332300561bb0037303
Ayoola <ayo.oloyede16@gmail.com>
2024-02-06 22:06:05 +0100

    Fix

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -2,13 +2,12 @@ const { Router } = require("express");
 const { userAuth } = require("../middlewares/userAuth");
 const cardController = require("../controllers/cardController");
 const { validateMongoIdParam } = require("../middlewares/validateMongoIdParam");
-const { checkFundingLimit } = require("../middlewares/checkFundingLimit");
 
 const router = Router();
 
 router.post("/create/:cardType", userAuth, cardController.create_new_card);
 router.get("/detail", userAuth, cardController.get_card_detail);
-router.post("/fund/:id", [userAuth, validateMongoIdParam, checkFundingLimit], cardController.fund_card);
+router.post("/fund/:id", [userAuth, validateMongoIdParam], cardController.fund_card);
 router.get("/transactions/:id", [userAuth, validateMongoIdParam], cardController.fetch_card_transactions);
 router.patch("/update-setting/:id/:type", [userAuth, validateMongoIdParam], cardController.update_card_setting);
 router.get("/single-card-detail/:id", [userAuth, validateMongoIdParam], cardController.get_single_card_detail);
