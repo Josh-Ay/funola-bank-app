1ef3e987296e149ba4d10b5d9e00b088539839dd
Ayoola <ayo.oloyede16@gmail.com>
2024-07-27 11:40:00 +0100

    Update API base url

--- services/config.js
+++ services/config.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseURL = 'https://yoola-bank-app.onrender.com';
-const baseURL = 'https://real-fly-hoodie.cyclic.app';
+// const baseURL = 'https://real-fly-hoodie.cyclic.app';
+const baseURL = 'https://funola.vercel.app';
 const countriesBaseURL = 'https://restcountries.com/v3.1/all';
 const convertCurrencyBaseURL = 'https://ayoolaa.pythonanywhere.com';
 
